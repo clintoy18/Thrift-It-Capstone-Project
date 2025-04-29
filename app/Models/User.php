e4bb8b9530c1448dd6e80aaa0560aa6344f8fef5
clintoy18 <aicsalonzo@gmail.com>
2025-04-30 03:06:41 +0800

    modified : User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
         'email',
         'role',
         'password',
+        'is_active',
     ];
 
     /**
@@ -47,6 +48,7 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'role' => 'integer',
+            'is_active' => 'boolean',
         ];
     }
 
