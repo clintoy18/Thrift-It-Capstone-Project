c32b31bd11fd32900509e646f5fbfeff9e728bbf
Jamaz <jbolongon12@gmail.com>
2025-07-15 13:26:28 +0800

    Saved my local changes before switching branches

--- resources/views/components/empty-message.blade.php
+++ resources/views/components/empty-message.blade.php
@@ -11,7 +11,7 @@
 
     @if (isset($link))
         <p class="mt-2 text-[#56432C]">Why not get started?</p>
-        <div class="mt-6">
+        <div class="mt-6 flex justify-center">
             <a href="{{ $link }}"
                class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-[#B59F84] transition-all duration-200 hover:bg-[#a08e77] hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
                 List your first product
