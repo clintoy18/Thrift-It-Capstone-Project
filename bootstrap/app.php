c81564b77c21ab2c9db07676ae39dc8452e3c8ed
clintoy18 <aicsalonzo@gmail.com>
2025-09-20 09:09:12 +0800

    create a middleware for subscription

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\Subscribed;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,8 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
        $middleware->alias([
-        'rolemiddleware'=>RoleMiddleware::class,
-       ]);
+            'rolemiddleware' => RoleMiddleware::class,
+            'subscribed' => Subscribed::class, 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
