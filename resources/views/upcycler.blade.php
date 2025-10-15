4987d63b450fe00e865289fd9ab22ef9eb046687
Jamaz <jbolongon12@gmail.com>
2025-10-15 19:06:01 +0800

    pushing the devdepencies

--- resources/views/upcycler.blade.php
+++ resources/views/upcycler.blade.php
@@ -1,22 +1,5 @@
 <x-app-layout>
-    <x-slot name="header">
-        <div class="flex items-center space-x-4">
-            <!-- Icon-like image -->
-            <div class="flex-shrink-0">
-                <svg class="w-8 h-8 text-green-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M12.586 4.586a2 2 0 112.828 2.828l-3 3a2 2 0 01-2.828 0 1 1 0 00-1.414 1.414 4 4 0 005.656 0l3-3a4 4 0 00-5.656-5.656l-1.5 1.5a1 1 0 101.414 1.414l1.5-1.5zm-5 5a2 2 0 012.828 0 1 1 0 101.414-1.414 4 4 0 00-5.656 0l-3 3a4 4 0 105.656 5.656l1.5-1.5a1 1 0 10-1.414-1.414l-1.5 1.5a2 2 0 11-2.828-2.828l3-3z" clip-rule="evenodd"></path>
-                </svg>
-            </div>
-            <div>
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    {{ __('Upcycler Dashboard') }}
-                </h2>
-                <p class="text-sm text-gray-600 dark:text-gray-400 mt-1">
-                    Transform waste into wonder • Create sustainable beauty • Redefine recycling
-                </p>
-            </div>
-        </div>
-    </x-slot>
+   
 
    <!-- Cinematic Hero Section -->
 <div class="relative bg-[#F4F2ED] overflow-hidden">
