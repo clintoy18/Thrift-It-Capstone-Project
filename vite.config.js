3da3f04db316114f3474ac51fc230754b68e4887
clintoy18 <aicsalonzo@gmail.com>
2025-10-29 16:52:08 +0800

    reference to env

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  server: {
+    host: '0.0.0.0', // allow LAN access
+    port: 5173,
+    hmr: {
+      host: process.env.VITE_HOST
+    },
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
 });
