d70ef1c00b9b059e65c2649fdcfdbce5357e1ec6
clintoy18 <aicsalonzo@gmail.com>
2025-10-29 16:49:46 +0800

    remove

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // allow LAN access
-    port: 5173,
-    hmr: {
-      host: '192.168.5.193', // 👈 your PC’s IPv4 (same as above)
-    },
-  },
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
