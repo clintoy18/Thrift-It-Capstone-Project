454be152b62b2914930ffd9be5f66f4a0cc0d885
Jamaz <jbolongon12@gmail.com>
2025-11-01 23:24:53 +0800

    fixing the functionality of image sending and nav bar

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // allow LAN access
-    port: 5173,
-    hmr: {
-      host: process.env.VITE_HOST
-    },
-  },
+  
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
