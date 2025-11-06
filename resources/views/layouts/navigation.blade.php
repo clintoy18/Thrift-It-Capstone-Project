2a731f649e46fc476d12080f45acf191f3fa6c62
clintoy18 <aicsalonzo@gmail.com>
2025-11-06 14:33:01 +0800

    hover dark

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -326,7 +326,7 @@
 
                     <!-- Hamburger Menu -->
                     <button @click="mobileMenuOpen = !mobileMenuOpen"
-                        class="p-2 rounded-md text-gray-700 dark:text-gray-200 hover:bg-gray-200 focus:outline-none">
+                        class="p-2 rounded-md text-gray-700 dark:text-gray-200 hover:bg-gray-200  dark:hover:bg-gray-800 focus:outline-none">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
