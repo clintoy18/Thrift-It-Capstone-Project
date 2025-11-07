58448f7844ea67bfdeedc1d8f70b1ca9ab502c93
clintoy18 <aicsalonzo@gmail.com>
2025-11-07 16:55:33 +0800

    empty state component

--- app/View/Components/EmptyState.php
+++ app/View/Components/EmptyState.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class Empty-state extends Component
+class EmptyState extends Component
 {
     /**
      * Create a new component instance.
