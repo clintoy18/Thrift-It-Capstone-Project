78bada488204abff820d10493fbf6e552b581607
clintoy18 <aicsalonzo@gmail.com>
2025-11-08 22:50:21 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-const host = process.env.VITE_HOST || 'localhost';
-const port = process.env.VITE_PORT || 5173;
-
 export default defineConfig({
   
   plugins: [
