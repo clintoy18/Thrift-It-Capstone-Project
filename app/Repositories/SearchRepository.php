453e520dad1b80adb619da584710eb39414dcd6c
clintoy18 <aicsalonzo@gmail.com>
2025-11-08 23:35:24 +0800

    optimize product searching to show only those available and approved

--- app/Repositories/SearchRepository.php
+++ app/Repositories/SearchRepository.php
@@ -10,27 +10,38 @@ use App\Models\User;
 class SearchRepository
 
 {
-    public function searchProducts(string $query, int $perPage = 10)
+    public function searchProducts(string $query, int $perPage = 10, ?int $selectedCategoryId = null, ?int $selectedBarangayId = null)
     {
-        return Product::where(function ($q) use ($query) {
-            $q->where('name', 'like', "%{$query}%")
-            ->orWhere('description', 'like', "%{$query}%")
-             ->orWhereHas('barangay',function($b) use ($query){
-                $b->where('name','like',"%{$query}%");
-             });
-        })
-        ->where('status', 'available')
-        ->paginate(10);
+        $productsQuery = Product::with(['category', 'user', 'barangay'])
+            ->where(function ($q) use ($query) {
+                $q->where('name', 'like', "%{$query}%")
+                    ->orWhere('description', 'like', "%{$query}%")
+                    ->orWhereHas('barangay', function ($b) use ($query) {
+                        $b->where('name', 'like', "%{$query}%");
+                    });
+            })
+            ->where(function ($q) {
+                $q->where('status', 'available')
+                    ->orWhere('approval_status', 'approved');
+            });
+
+        // Optional category filter
+        if ($selectedCategoryId) {
+            $productsQuery->where('category_id', $selectedCategoryId);
+        }
+
+        // Optional barangay filter
+        if ($selectedBarangayId) {
+            $productsQuery->where('barangay_id', $selectedBarangayId);
+        }
+
+        return $productsQuery->paginate($perPage);
     }
 
     public function searchUsers(string $query, int $perPage = 10)
     {
         return User::where('fname', 'like', "%{$query}%")
-        ->orWhere('email', 'like', "%{$query}%")
-        ->paginate(10);
-
+            ->orWhere('email', 'like', "%{$query}%")
+            ->paginate(10);
     }
-  
-
 }
-
