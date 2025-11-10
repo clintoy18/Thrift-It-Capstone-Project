56acd6e85d2fe723cb630a9753cf444ba9d6cad5
clintoy18 <aicsalonzo@gmail.com>
2025-11-10 10:22:41 +0800

    s3 on more from this user

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -737,9 +737,8 @@
                                             @endif
 
                                             <div class="relative aspect-square overflow-hidden">
-                                                <img src="{{ asset('storage/' . $product->first_image) }}"
+                                                <img src="{{ Storage::disk('s3')->url($image->image) }}"
                                                     alt="{{ $product->name }}" class="w-full h-full object-cover">
-
                                                 <div
                                                     class="absolute inset-0 bg-gray-800 bg-opacity-20 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
 
