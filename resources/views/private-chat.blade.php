302c279a79fbfd6704cc0f179a8c5a2c42a0d52a
clintoy18 <aicsalonzo@gmail.com>
2025-11-28 18:41:36 +0800

    remove AudioCall and VideoCall

--- resources/views/private-chat.blade.php
+++ resources/views/private-chat.blade.php
@@ -209,7 +209,7 @@
                                 </div>
                             </div>
                             <div class="flex items-center space-x-2">
-                            
+                            <!-- 
                                 <button onclick="startAudioCall()" class="p-2 text-white hover:bg-white hover:bg-opacity-20 rounded-full transition-colors" title="Start Audio Call">
                                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
@@ -220,7 +220,7 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"></path>
                                     </svg>
                                 </button>
-                                  
+                            -->
                                 <!-- Settings Dropdown - Desktop Only -->
                                 <div id="chat-settings-dropdown" class=" lg:block relative">
                                     <button id="settings-toggle-btn" 
